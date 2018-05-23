ebbb923765c1beff6f2861b151edc27e83a8ed93
kubil <kubil@DESKTOP-UIDHA81>
2018-05-24 00:07:08 +0300

    Add project files.

---
+++ SortAlgorithmSimulator/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace SortAlgorithmSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddNumber = new System.Windows.Forms.Button();
+            this.pnlAddNumber = new System.Windows.Forms.Panel();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.btnOkNumber = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.pnlAddNumber.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.pnlAddNumber);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(122, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 357);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.btnstart);
+            this.panel2.Controls.Add(this.btnAddNumber);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(122, 357);
+            this.panel2.TabIndex = 1;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(59, 21);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(33, 20);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number :";
+            // 
+            // btnAddNumber
+            // 
+            this.btnAddNumber.Location = new System.Drawing.Point(12, 32);
+            this.btnAddNumber.Name = "btnAddNumber";
+            this.btnAddNumber.Size = new System.Drawing.Size(104, 23);
+            this.btnAddNumber.TabIndex = 2;
+            this.btnAddNumber.Text = "Add Number";
+            this.btnAddNumber.UseVisualStyleBackColor = true;
+            this.btnAddNumber.Click += new System.EventHandler(this.btnAddNumber_Click);
+            // 
+            // pnlAddNumber
+            // 
+            this.pnlAddNumber.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlAddNumber.Controls.Add(this.btnOkNumber);
+            this.pnlAddNumber.Controls.Add(this.txt6);
+            this.pnlAddNumber.Controls.Add(this.txt5);
+            this.pnlAddNumber.Controls.Add(this.txt4);
+            this.pnlAddNumber.Controls.Add(this.txt3);
+            this.pnlAddNumber.Controls.Add(this.txt2);
+            this.pnlAddNumber.Controls.Add(this.label1);
+            this.pnlAddNumber.Controls.Add(this.txt1);
+            this.pnlAddNumber.Location = new System.Drawing.Point(3, 3);
+            this.pnlAddNumber.Name = "pnlAddNumber";
+            this.pnlAddNumber.Size = new System.Drawing.Size(299, 80);
+            this.pnlAddNumber.TabIndex = 0;
+            this.pnlAddNumber.Visible = false;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(98, 21);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(33, 20);
+            this.txt2.TabIndex = 2;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(137, 21);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(33, 20);
+            this.txt3.TabIndex = 3;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(176, 21);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(33, 20);
+            this.txt4.TabIndex = 4;
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(215, 21);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(33, 20);
+            this.txt5.TabIndex = 5;
+            this.txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(254, 21);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(33, 20);
+            this.txt6.TabIndex = 6;
+            this.txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnOkNumber
+            // 
+            this.btnOkNumber.Location = new System.Drawing.Point(6, 54);
+            this.btnOkNumber.Name = "btnOkNumber";
+            this.btnOkNumber.Size = new System.Drawing.Size(75, 23);
+            this.btnOkNumber.TabIndex = 8;
+            this.btnOkNumber.Text = "Okey";
+            this.btnOkNumber.UseVisualStyleBackColor = true;
+            this.btnOkNumber.Click += new System.EventHandler(this.btnOkNumber_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 8);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "BubleSort";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "QuickSort";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // btnstart
+            // 
+            this.btnstart.Enabled = false;
+            this.btnstart.Location = new System.Drawing.Point(12, 169);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(104, 23);
+            this.btnstart.TabIndex = 5;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Yellow;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1.Location = new System.Drawing.Point(62, 154);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(55, 52);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Visible = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn2.Location = new System.Drawing.Point(155, 154);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(55, 52);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Visible = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn3.Location = new System.Drawing.Point(248, 154);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(55, 52);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Visible = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn4.Location = new System.Drawing.Point(341, 154);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(55, 52);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Visible = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn5.Location = new System.Drawing.Point(434, 154);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(55, 52);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Visible = false;
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Silver;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn6.Location = new System.Drawing.Point(527, 154);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(55, 52);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.radioButton2);
+            this.panel3.Controls.Add(this.radioButton1);
+            this.panel3.Location = new System.Drawing.Point(4, 78);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(97, 76);
+            this.panel3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 357);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Simulation";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.pnlAddNumber.ResumeLayout(false);
+            this.pnlAddNumber.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlAddNumber;
+        private System.Windows.Forms.Button btnOkNumber;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnAddNumber;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ SortAlgorithmSimulator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortAlgorithmSimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddNumber_Click(object sender, EventArgs e)
+        {
+            pnlAddNumber.Visible = true;
+        }
+
+        private void btnOkNumber_Click(object sender, EventArgs e)
+        {
+            pnlAddNumber.Visible = false;
+            btn1.Text = txt1.Text;
+            btn2.Text = txt2.Text;
+            btn3.Text = txt3.Text;
+            btn4.Text = txt4.Text;
+            btn5.Text = txt5.Text;
+            btn6.Text = txt6.Text;
+
+            btn1.Visible = true;
+            btn2.Visible = true;
+            btn3.Visible = true;
+            btn4.Visible = true;
+            btn5.Visible = true;
+            btn6.Visible = true;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked || radioButton2.Checked)
+                btnstart.Enabled = true;
+        }
+        public void sortBuble()
+        {
+            int n1 = Convert.ToInt32(txt1.Text);
+            int n2 = Convert.ToInt32(txt2.Text);
+            //int n3 = Convert.ToInt32(txt3.Text);
+            //int n4 = Convert.ToInt32(txt4.Text);
+            //int n5 = Convert.ToInt32(txt5.Text);
+            //int n6 = Convert.ToInt32(txt6.Text);
+
+            int temp;
+            int x = 0, y = 0;
+            if (n1 < n2)
+            {
+                x = txt1.Location.X;
+                y = txt1.Location.Y;
+                txt1.Location = txt2.Location;
+                txt2.Location = new Point(x, y);
+            }
+                
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            sortBuble();
+        }
+    }
+}
